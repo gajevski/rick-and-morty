7a40579772cd3fa56c7c0dd066b071a61a7bc261
gajevski <mikolajxgajewski@gmail.com>
2021-03-03 23:15:06 +0100

    Update hero section design

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [HeroSectionComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FlexLayoutModule
   ]
 })
 export class HomeModule { }
